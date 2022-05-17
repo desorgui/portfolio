2226438af62bb83b3cdab3233fff2a753e7516cd
desorgui <desorguishny@gmail.com>
2022-05-17 12:45:03 -0400

    Add Mobile popup window

--- custom.js
+++ custom.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector('#nav-button');
 const closebtn = document.querySelector('.btn-close');
+const seeproject = document.querySelector('.see-project-btn');
 const mobileNav = document.querySelector('.mobile-nav');
+const popup = document.querySelector('#popup');
 hamburger.addEventListener('click', () => {mobileNav.classList.add('active'); });
 closebtn.addEventListener('click', () => {mobileNav.classList.remove('active'); });
+seeproject.addEventListener('click', () => {popup.classList.add('active'); });
