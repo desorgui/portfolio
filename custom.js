24f56e23ddf8dd37f56b0215d442f5a377c11b77
desorgui <desorguishny@gmail.com>
2022-05-17 12:41:54 -0400

    Add event listener mobile menu

--- custom.js
+++ custom.js
@@ -1,3 +1,5 @@
 const hamburger = document.querySelector('#nav-button');
-let mobileNav = document.querySelector('.mobile-nav');
+const closebtn = document.querySelector('.btn-close');
+const mobileNav = document.querySelector('.mobile-nav');
 hamburger.addEventListener('click', () => {mobileNav.classList.add('active'); });
+closebtn.addEventListener('click', () => {mobileNav.classList.remove('active'); });
