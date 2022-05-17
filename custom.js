cd4a10c2dc9f1872053a6c2840171ca3ca97e397
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-17 17:04:26 +0000

    Add event listener to mobile menu items

--- custom.js
+++ custom.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('#nav-button');
-const closebtn = document.querySelector('.btn-close');
+const closeBtn = document.querySelector('.btn-close');
 const mobileNav = document.querySelector('.mobile-nav');
+const menuItem = document.querySelector('.mb-item');
 hamburger.addEventListener('click', () => {mobileNav.classList.add('active'); });
-closebtn.addEventListener('click', () => {mobileNav.classList.remove('active'); });
+closeBtn.addEventListener('click', () => {mobileNav.classList.remove('active'); });
+menuItem.addEventListener('click', () => {mobileNav.classList.remove('active');});
