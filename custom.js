b9c097ca2280d2e337018a10403f1ef956c252a2
desorgui <desorguishny@gmail.com>
2022-05-17 13:27:25 -0400

    add event listener for each menu option

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('#nav-button');
 const closeBtn = document.querySelector('.btn-close');
 const mobileNav = document.querySelector('.mobile-nav');
-const menuItem = document.querySelector('.mb-item');
+const menuItems = document.querySelectorAll('.mb-item');
 hamburger.addEventListener('click', () => {mobileNav.classList.add('active'); });
 closeBtn.addEventListener('click', () => {mobileNav.classList.remove('active'); });
-menuItem.addEventListener('click', () => {mobileNav.classList.remove('active');});
+menuItems.forEach(el => el.addEventListener('click',() => {mobileNav.classList.remove('active'); }));
