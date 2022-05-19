b94c98a1278dd5ab85b2a2006d9b821160aaef9c
desorgui <desorguishny@gmail.com>
2022-05-19 14:39:09 -0400

    Add change required by reviewer

--- custom.js
+++ custom.js
@@ -15,7 +15,7 @@ menuItems.forEach((el) => el.addEventListener('click', () => {
 
 const WorkData = [
   {
-    name: 'Keeping track of hundreds  of components website',
+    name: 'Multi Post Stories',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem  ',
     image: './images/Img-Placeholder.png',
     technologies: ['HTML', 'CSS'],
